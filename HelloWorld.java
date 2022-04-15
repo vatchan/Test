c5134b298525a835236fd88b3ac45a82b2ac6f19
Srivatchan <vatchan1234@gmail.com>
2022-04-15 16:01:07 +0530

    Object Customer DB

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,31 @@
+import java.sql.Date;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of times to print message :");
-        int number = sc.nextInt();
-        while(number > 0) {
-            System.out.println("Hello World");
-            number--;
-        }
+      Customer customer = null;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter number of Customers:");
+        LinkedList<Customer> listOfCustomers= new LinkedList<>();
+      int count = sc.nextInt();
+      while(count>0) {
+          System.out.println("Enter Name:");
+          String customerName = sc.next();
+          System.out.println("Enter number:");
+          long customerPhoneNo = sc.nextLong();
+          System.out.println("Enter Address:");
+          String customerAddress = sc.next();
+          System.out.println("Enter DOB:");
+          long dob = Long.parseLong(sc.next());
+          Date customerDOB = new Date(dob);
+          customer = new Customer(customerName, customerPhoneNo, customerAddress, customerDOB);
+          listOfCustomers.add(customer);
+          count--;
+      }
+      for(Customer cust : listOfCustomers)
+      {
+          System.out.println(cust.toString());
+      }
     }
 }
