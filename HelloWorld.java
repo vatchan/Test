d089e92321519102eb4f8370dd6e692947c8955e
Srivatchan <vatchan1234@gmail.com>
2022-04-15 15:26:36 +0530

    Modify HW Program1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of times to print message :");
+        int number = sc.nextInt();
+        while(number > 0) {
+            System.out.println("Hello World");
+            number--;
+        }
     }
 }
